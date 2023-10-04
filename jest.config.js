8de6294c2203740c483e0c5f4ee59809990bc1f0
John Parks <john.m.parks@gmail.com>
2023-10-03 17:10:42 -0700

    Removes unused Jest preset due to ts-jest transform as-per Jest docs recommendation

--- jest.config.js
+++ jest.config.js
@@ -8,19 +8,6 @@ export default {
         'src/lib/**/*.ts'
     ],
 
-    /////////////////////////////////////////////////////////////////////////////
-
-    // A preset that is used as a base for Jest's configuration. A preset should point to an npm
-    //    module that has a jest-preset.json, jest-preset.js, jest-preset.cjs or jest-preset.mjs
-    //       file at the root
-    // - "ts-jest" for no ESM support
-    // - "ts-jest/presets/default-esm" for ESM support
-    preset : 'ts-jest/presets/default-esm', // ts-jest with ESM support (allows for node_module imports)
-
-    /////////////////////////////////////////////////////////////////////////////
-
-    // Following "ESM Support" configuration example for Jest/TypeScript
-    // - https://kulshekhar.github.io/ts-jest/docs/next/guides/esm-support
 
     // Enable useESM true for ts-jest config
     transform: {
